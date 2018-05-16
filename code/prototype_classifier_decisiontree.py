36c7c5243b67849f5c60f064df696b3e56f025f1
Tiffany Chen <tiffany@tiffanys-mbp.devices.brown.edu>
2018-05-16 14:38:10 -0400

    more commenting and solving some module stuff

--- code/prototype_classifier_decisiontree.py
+++ code/prototype_classifier_decisiontree.py
@@ -12,13 +12,13 @@ def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=
     columnwidth = max([len(x) for x in labels] + [5])  # 5 is value length
     empty_cell = " " * columnwidth
     # Print header
-    print("    " + empty_cell, end=" ")
+    print("    " + empty_cell, " ")
     for label in labels:
-        print("%{0}s".format(columnwidth) % label, end=" ")
+        print("%{0}s".format(columnwidth) % label, " ")
     print()
     # Print rows
     for i, label1 in enumerate(labels):
-        print("    %{0}s".format(columnwidth) % label1, end=" ")
+        print("    %{0}s".format(columnwidth) % label1, " ")
         for j in range(len(labels)):
             cell = "%{0}.1f".format(columnwidth) % cm[i, j]
             if hide_zeroes:
@@ -27,7 +27,7 @@ def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=
                 cell = cell if i != j else empty_cell
             if hide_threshold:
                 cell = cell if cm[i, j] > hide_threshold else empty_cell
-            print(cell, end=" ")
+            print(cell, " ")
         print()
 
 # trainingData is the csv with calibration data
